553ed33f427bc61160298a375a649752f0766f31
AlexRatiu <117086170+AlexRatiu@users.noreply.github.com>
2022-11-01 19:05:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -166,12 +166,6 @@ int main()
 
     Matrice m1 =  Matrice(n,m,x);
     Matrice m2 =  Matrice(n,m,y);
-    Matrice m3 = Matrice(3,3,3);
-    int b,c;
-    b=m1.getNrlinii();
-    c=m1.getNrColoane();
-    m3.setNrColoane(b);
-    m3.setNrLinii(c);
 
     switch(a)
     {
