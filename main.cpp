3df275592447f5fd835f0b968437d93e0c9b5299
AlexRatiu <117086170+AlexRatiu@users.noreply.github.com>
2022-11-01 19:52:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,33 @@ class Matrice
 {
 private:
     int nrLinii,nrColoane;
-    int **m = new int*[20];
+    int **m;
+    void allocMatrix()
+    {
+       m= new int *[nrLinii];
+       for(int i=0; i<nrLinii; i++)
+            m[i]=new int [nrColoane];
+    }
 
 public:
-    Matrice(int nrLinii, int nrColoane,int x)
+    Matrice(int nrL, int nrC,int x)
     {
-        this->nrLinii = nrLinii;
-        this->nrColoane = nrColoane;
-        for (int i = 0; i < this->nrLinii; i++)
-            m[i] = new int[this->nrColoane];
-        for(int i = 0; i<this->nrLinii; i++)
-            for(int j = 0; j<this->nrColoane; j++)
+
+        nrLinii=nrL;
+        nrColoane=nrC;
+        allocMatrix();
+        for(int i=0;i<nrLinii;i++)
+            for(int j=0;j<nrColoane;j++)
                 m[i][j] = x;
+
+
     }
 
     Matrice()
     {
-        this->nrLinii = 1;
-        this->nrColoane = 1;
-        for (int i = 0; i < nrLinii; ++i)
-            m[i] = new int[nrColoane];
+        nrLinii = 1;
+        nrColoane = 1;
+        allocMatrix();
         for(int i = 0; i<nrLinii; i++)
             for(int j = 0; j<nrColoane; j++)
                 m[i][j] = 1;
